dccc905217736e4b0baaf9cd78f0ef3894096901
jegarba <jegarba1996@gmail.com>
2020-12-10 11:52:07 -0500

    terminado, funcionando con el properties las clases dinamicas

--- src/main/java/com/clearminds/jg/model/PersonaManager.java
+++ src/main/java/com/clearminds/jg/model/PersonaManager.java
@@ -1,15 +1,33 @@
 package com.clearminds.jg.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.clearminds.jg.excepciones.InstaceException;
 
 public class PersonaManager {
 
 	ServicioPersona serv;
 
+
+	
 	public PersonaManager() throws InstaceException {
+		
+		File f=new File("config.properties");
+		System.out.println("ruta:"+f.getAbsoluteFile());
+		Properties p = new Properties();
+		
+		
 		try {
-			Class<?> clase = Class.forName("com.clearminds.jg.model.ServicioPersonaBDD");
+			p.load(new FileReader(f.getAbsoluteFile()));
+			System.out.println(p.getProperty("servicio"));
+			
+			Class<?> clase = Class.forName(p.getProperty("servicio"));
 			serv = (ServicioPersona) clase.newInstance();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new InstaceException("Error al obtener una instancia de ServicioPersona");
